1eee2ba19ff1cdab700d5a20a5af7398dfc12afe
sA1mon <loner40466@gmail.com>
2020-03-21 16:17:24 +0300

    Переход на netHttpBinding

--- Host/Program.cs
+++ Host/Program.cs
@@ -21,18 +21,26 @@ namespace Host
 
     internal static class Program
     {
+        private static void WriteLineWithTime(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}] {msg}");
+        }
+
         private static void Main()
         {
             using (var host = new ServiceHost(typeof(Chat)))
             {
                 var server = new Callback();
 
-                host.AddServiceEndpoint(typeof(ChatApp.IChat), new NetTcpBinding {PortSharingEnabled = true},
-                    "net.tcp://192.168.0.104:8731/");
+                host.AddServiceEndpoint(typeof(ChatApp.IChat), new NetHttpBinding(), "http://192.168.0.104:8731/");
+                //host.AddServiceEndpoint(typeof(ChatApp.IChat), new NetTcpBinding {PortSharingEnabled = true},
+                //    "net.tcp://192.168.0.104:8731/");
                 host.Open();
                 
-                Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}] Host was started");
-                //Console.ReadLine();
+                WriteLineWithTime("Host was started");
+                WriteLineWithTime("Press any key to log in as Admin");
+                Console.ReadKey();
+                WriteLineWithTime("You are logged in");
                 var client = new ChatClient(new InstanceContext(server));
                 var user = client.Add("Admin");
 
@@ -45,14 +53,14 @@ namespace Host
                         case "!exit":
                         {
                             client.Remove(user);
-                            Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}] Wait... saving results");
+                            WriteLineWithTime("Wait... saving results");
                             client.Shutdown(true);
                             return;
                         }
                         case "!exit --force":
                         {
                             client.Remove(user);
-                            Console.WriteLine($"[{DateTime.Now.ToLongTimeString()}] Wait... saving results");
+                            WriteLineWithTime("Wait... stop the server");
                             client.Shutdown(false);
                             return;
                         }
